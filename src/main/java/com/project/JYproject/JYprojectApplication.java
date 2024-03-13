84aec1dc8f72aed93311f7dbde379a1166f29ba7
LEEjunswer <jun_5949@naver.com>
2024-03-13 21:44:34 +0900

    체크용
    체크용

---
+++ src/main/java/com/project/JYproject/JYprojectApplication.java
@@ -0,0 +1,13 @@
+package com.project.JYproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JYprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JYprojectApplication.class, args);
+	}
+
+}
