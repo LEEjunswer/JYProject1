5c2fb334351eed8446667be510ccc1dd223eca49
LEEjunswer <jun_5949@naver.com>
2024-03-22 15:11:48 +0900

    35
    35

--- src/main/java/com/JYProject/project/controller/ReplyController.java
+++ src/main/java/com/JYProject/project/controller/ReplyController.java
@@ -8,10 +8,11 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
 public class ReplyController {
-
+    /*  나중에 apiController 옮기고 수정할 예정  ajax 로 전부 받을 예쩡*/
     private final ReplyServiceImpl replyService;
     @Autowired
     public ReplyController(ReplyServiceImpl replyService){
@@ -19,13 +20,29 @@ public class ReplyController {
     }
 
     @PostMapping("/boards/content/{idx}/reply")
-    public String create(@PathVariable("idx") Long idx, HttpSession session, @ModelAttribute ReplyDTO replyDTO){
+    public String create(@PathVariable("idx") Long idx, HttpSession session, @ModelAttribute ReplyDTO replyDTO, RedirectAttributes redirectAttributes){
         MemberDTO m = (MemberDTO)session.getAttribute("log");
         Long writer =  (Long)m.getId();
+        if(writer == null ){
+            redirectAttributes.addFlashAttribute("error","로그인상태 아님 잘못된 접근입니다");
+            return "redirect:/members/home";
+        }
         replyDTO.setBoardNo(idx);
         replyDTO.setWriter(writer);
         int check = replyService.insertReply(replyDTO);
+        redirectAttributes.addFlashAttribute("suc", "댓글이 정상적으로 달렸습니다");
     return "redirect:/boards/content/" +idx;
     }
-    
+
+    @PostMapping("boards/content/{idx}/reply/{delReplyIdx}")
+    public String delelteReply(@PathVariable("idx") Long idx, @PathVariable("delReplyIdx") Long replyIdx,HttpSession session){
+        MemberDTO m = (MemberDTO)session.getAttribute("log");
+        Long writer = (Long)m.getId();
+        if(writer== null){
+           return  "members/home";
+        }
+        replyService.deleteReply(replyIdx);
+
+        return "redirect:boards/content"+idx;
+    }
 }
